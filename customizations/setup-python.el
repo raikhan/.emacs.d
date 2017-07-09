8bd2d6709c576f8bed7672bdeedb1772d7370ee9
raikhan <raikhan@users.noreply.github.com>
2017-07-09 18:33:04 +0800

    renamed setup-python to .el

---
+++ customizations/setup-python.el
@@ -0,0 +1,38 @@
+;;
+;; Python mode setup
+;;
+
+;;
+;; Elpy
+;;
+
+;; set PATH inside Emacs (should be done before loading elpy
+(exec-path-from-shell-copy-env "PATH")
+
+;; enable elpy mode for Python
+(elpy-enable)
+
+;; needed to use ipython as python shell in Emacs
+(setenv "IPY_TEST_SIMPLE_PROMPT" "1")
+(elpy-use-ipython)
+
+(setq elpy-rpc-backend "jedi")
+
+;; setup personal python mode
+(defun personal-python-mode-defaults ()
+  "Personal defaults for Python programming."
+  ;; Enable elpy mode
+  (elpy-mode)
+  ;; Jedi backend                                                                                       
+  ;; (jedi:setup)
+  ;; (setq jedi:complete-on-dot t) ;optional                                                               
+  ;; (auto-complete-mode)
+  ;; (jedi:ac-setup)
+  (setq elpy-rpc-python-command "python3") 
+  (python-shell-interpreter "ipython3")
+  )
+
+(setq personal-python-mode-hook 'personal-python-mode-defaults)
+
+(add-hook 'python-mode-hook (lambda ()
+                              (run-hooks 'personal-python-mode-hook)))
