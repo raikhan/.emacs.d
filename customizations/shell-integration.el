321f4b2ba379c83c313bfcda7f7a284ca338ef10
raikhan <raikhan@users.noreply.github.com>
2019-05-19 12:23:50 +0800

    added PYTHONPATH to exec-path-from-shell

--- customizations/shell-integration.el
+++ customizations/shell-integration.el
@@ -1,6 +1,8 @@
 ;; Sets up exec-path-from shell
 ;; https://github.com/purcell/exec-path-from-shell
-(when (memq window-system '(mac ns))
+(when (memq window-system '(mac ns x))
   (exec-path-from-shell-initialize)
   (exec-path-from-shell-copy-envs
-   '("PATH")))
+   '("PATH"))
+  (exec-path-from-shell-copy-envs
+   '("PYTHONPATH")))
