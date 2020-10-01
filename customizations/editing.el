6a19e263dd20e66d1dbdc61e66332fa1632c7cd3
Milan Raicevic <raikhan@gmail.com>
2020-10-02 01:09:21 +0800

    cleaned up saveplace in editing.el

--- customizations/editing.el
+++ customizations/editing.el
@@ -74,10 +74,12 @@
 ;; When you visit a file, point goes to the last place where it
 ;; was when you previously visited the same file.
 ;; http://www.emacswiki.org/emacs/SavePlace
-(require 'saveplace)
-(setq-default save-place t)
-;; keep track of saved places in ~/.emacs.d/places
-(setq save-place-file (concat user-emacs-directory "places"))
+(use-package saveplace
+  :config
+  (setq save-place-file (concat user-emacs-directory "places"))  ; keep track of saved places in ~/.emacs.d/places
+  (setq save-place-forget-unreadable-files nil)                  ; make sure unreadable files are not recorded, makes emacs closing much faster
+  (save-place-mode 1)
+)
 
 ;; Emacs can automatically create backup files. This tells Emacs to
 ;; put all backups in ~/.emacs.d/backups. More info:
